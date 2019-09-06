8a4e9e0f7e1f6b75e38c8059ab4cd8d5af61b202
Luke Holoubek <lukasholoubek@gmail.com>
2019-09-06 06:22:26 -0700

    image import

--- tasks/luke/ch_1.py
+++ tasks/luke/ch_1.py
@@ -1,4 +1,8 @@
 from ch_1_the_field import plotting, image, png
+import os
+
+repo_path = os.path.expanduser("~") + "/Documents/coding_the_matrix"
+image_path = repo_path + "/support/ch_1_the_field/img01.png"
 
 S = {2 + 2j, 3 + 2j, 1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 +1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j}
 
@@ -18,8 +22,18 @@ def task_1_4_7():
     print(transformed)
     plotting.plot(transformed, 6)
 
+def task_1_4_9():
+    transform = .5j
+    transformed = {transform * z for z in S}
+    plotting.plot(transformed, 6) 
 
+def task_1_4_10():
+    imported = image.file2image(image_path)
+    print("imported image")
+    
 if __name__ == "__main__":
     # task_1_4_1()
     # task_1_4_3()
-    task_1_4_7()
+    # task_1_4_7()
+    # task_1_4_9()
+    task_1_4_10()
